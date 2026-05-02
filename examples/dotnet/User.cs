b17d4192feb788419880385b9c4029fe30114899
darkest-x <106761463+darkest-x@users.noreply.github.com>
2026-05-03 01:00:07 +0800

    Add UpdatedAt property to User class

--- examples/dotnet/User.cs
+++ examples/dotnet/User.cs
@@ -60,6 +60,7 @@ namespace TestMind.Models
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         /// <summary>
+
         /// 更新时间
         /// </summary>
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
